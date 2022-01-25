7c1e1b726a7604812eb76839183a64e2e3e339bd
mohamed1408 <mohamedanastsi@gmail.com>
2022-01-25 23:10:51 +0530

    init push

---
+++ src/app/services/globals/index.ts
@@ -0,0 +1,18 @@
+import { EventEmitter, Injectable, Output } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ConstantsService {
+    @Output() globals = new EventEmitter();
+    isloggedin: boolean = false;
+    stores = [];
+    companyid = 0;
+    constructor() { }
+    listener() {
+        return this.globals;
+    }
+    emit(data) {
+        this.globals.emit(data)
+    }
+}
